6a549607068fc037e68bcd5116dc060275d7bfa0
Keerthana Manoharan <kmanohar@redhat.com>
2025-11-20 14:40:11 +0530

    Increasing the timeout
    Signed-off-by: Keerthana Manoharan <kmanohar@redhat.com>

--- ocs_ci/helpers/dr_helpers_ui.py
+++ ocs_ci/helpers/dr_helpers_ui.py
@@ -1023,7 +1023,7 @@ def check_dr_status(
         if not (expected_status == "FailingOver" or expected_status == "Relocating"):
             log.info("Verifying DR status on UI...")
             wait_for_text_result = TimeoutSampler(
-                timeout=320,
+                timeout=600,
                 sleep=10,
                 func=acm_obj.wait_until_expected_text_is_found,
                 locator=acm_loc["dr-status"],
@@ -1037,6 +1037,7 @@ def check_dr_status(
                 )
                 current_status = acm_obj.get_element_text(acm_loc["dr-status"])
                 log.info(f"Current status is {current_status}")
+                acm_obj.take_screenshot()
                 raise ResourceWrongStatusException
 
             log.info(
@@ -1119,6 +1120,7 @@ def check_dr_status(
                 f"current popover message is {current_pop_over_text} "
                 f"but the expected is {expected_status_popover_messages[expected_status]}"
             )
+            acm_obj.take_screenshot()
             raise UnexpectedBehaviour
 
         log.info("Popover message validation is successful")
